6aeb5f80aac93aad42821fc699eb59275ef68e67
faraz <farazn8564@gmail.com>
2025-04-18 10:04:37 +0330

    all music class with stream

--- src/general/Music.java
+++ src/general/Music.java
@@ -2,6 +2,8 @@ package general;
 
 import java.util.ArrayList;
 
+import javax.imageio.plugins.tiff.TIFFDirectory;
+
 public class Music {
 
     private String title;
@@ -12,6 +14,7 @@ public class Music {
     public Music(String title, User singer) throws InvalidOperationException {
         setSinger(singer);
         setTitle(title);
+        ifMusicExist(title, singer.getUsername());
         numberOfStream = 0;
 
         allMusics.add(this);
@@ -22,6 +25,7 @@ public class Music {
             throw new InvalidOperationException("Provided Title is empty");
 
         }
+
         this.title = title;
     }
     public void setSinger(User user) throws InvalidOperationException {
@@ -30,8 +34,30 @@ public class Music {
         }
         this.singer = user;
     }
+    public String getTitle() {return title;}
+    public User getSinger() {return singer;}
 
+    public void play() {
+        ++numberOfStream;
+        System.out.println("---------------");
+        System.out.println("Music :  " + title + "\n Singer : " + singer.getUsername() + "\n  Streams : " + numberOfStream);
+    }
 
+    private void ifMusicExist(String title, String singer) throws InvalidOperationException {
+        for(Music music : allMusics) {
+            if(music.getTitle() == title && music.getSinger().getUsername().equals(singer)) {
+                throw new InvalidOperationException("Music with title : " + title + "  singer Name : " + singer + "  is already exist");
+            }
+        }
+    }
+    public ArrayList<Music> search(String title) {
+        ArrayList<Music> musics = new ArrayList<>();
+        
+        allMusics.stream().filter(music -> music.getTitle().equals(title)).forEach(music -> musics.add(music));
 
-    
+        return musics;
+    }
+    public Music search(String title, String singer) {
+       return allMusics.stream().filter(music -> music.getTitle().equals(title) && music.getSinger().getUsername().equals(singer)).findAny().orElse(null);
+    }    
 }
